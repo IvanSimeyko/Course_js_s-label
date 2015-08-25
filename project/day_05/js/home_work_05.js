02d6e28a56e485772ba84776d1a5aa5724dcfa5b
Ivan Simeyko <iv.simeyko@gmail.com>
2015-08-25 22:24:11 +0300

    День 05 - домашка 05: сделал отдельный .js для доиашки

---
+++ project/day_05/js/home_work_05.js
@@ -0,0 +1,147 @@
+// Решение домашки
+// Прототип объектов
+var proto = {
+    background: 'pink',
+
+    render: function() {
+        console.log('Выводим фигуру на страницу');
+        this.updateStyle();
+        document.body.appendChild(this.element);
+    },
+
+    changeColor: function(color) {
+       console.log('Меняем цвет у фигуры');
+       this.element.style.background = color;
+        //this.updateStyle();
+    },
+
+    move: function(where) {
+        console.log('Перемещаем фигуру');
+        var tempSize = 0;
+        switch(where) {
+          case 'down':
+            tempSize =  parseInt(this.element.style.top, 10);
+            tempSize = tempSize + 100;
+            this.element.style.top = tempSize + 'px';
+            break;
+          case 'up':
+            tempSize =  parseInt(this.element.style.top, 10);
+            tempSize = tempSize - 100;
+            this.element.style.top = tempSize + 'px';
+            break;
+          case 'left':
+            tempSize =  parseInt(this.element.style.left, 10);
+            tempSize = tempSize - 100;
+            this.element.style.left = tempSize + 'px';
+            break;
+          case 'rigth':
+          default:
+            tempSize =  parseInt(this.element.style.left, 10);
+            tempSize = tempSize + 100;
+            this.element.style.left = tempSize + 'px';
+        }
+    }
+};
+
+// Конструктоор объекта квадрат
+function Square() {
+    this.width = '100px';
+    this.height = '100px';
+    //this.color = 'red';
+    this.position = 'absolute';
+    this.top = '50px';
+    this.left = '450px';
+    this.element = document.createElement('div');
+
+    this.updateStyle = function () {
+        var squareDiv = this.element;
+
+        squareDiv.style.width = this.width;
+        squareDiv.style.height = this.height;
+        squareDiv.style.background = this.background;
+        squareDiv.style.color = this.color;
+        squareDiv.style.position = this.position;
+        squareDiv.style.top = this.top;
+        squareDiv.style.left = this.left;
+
+       document.body.appendChild(squareDiv)
+    };
+}
+
+// Назначаем прототип
+Square.prototype = proto;
+
+//Конструктор объекта круг
+function Circle() {
+    this.width = '100px';
+    this.height = '100px';
+    this.border = '50px';
+    this.position = 'absolute';
+    this.top = '50px';
+    this.left = '150px';
+    this.id = 'circle';
+    this.element = document.createElement( 'div' );
+
+    this.updateStyle = function() {
+        var circleDiv = this.element;
+
+        circleDiv.style.width = this.width;
+        circleDiv.style.height = this.height;
+        circleDiv.style.background = this.background;
+        circleDiv.style.borderRadius = this.border;
+        circleDiv.style.position = this.position;
+        circleDiv.style.top = this.top;
+        circleDiv.style.left = this.left;
+        circleDiv.style.id= this.id;
+
+        document.body.appendChild(circleDiv)
+    };
+}
+
+function changeColorForAll(color) {
+    console.log('меняем цвет у двух фигур');
+
+    shapes.forEach(function (shape) {
+        shape.changeColor(color);
+    });
+}
+
+//Назначаем прототип
+Circle.prototype = proto;
+
+//Создаем объекты
+var square = new Square();
+var circle = new Circle();
+var shapes = [square, circle];
+
+square.render();
+circle.render();
+
+circle.move('down');
+square.move('down');
+
+square.changeColor('red');
+circle.changeColor('red');
+
+changeColorForAll('black');
+/*
+//Анимация и таймеры (разбор темы)
+var delay = 1000;
+i = 0;
+
+var startTimer = function() {
+    if (i < 5) {
+        console.log( 'функция startTimer сработала ' + i + ' раз' );
+        setTimeout(startTimer, delay);
+    }
+    i++;
+};
+
+var timer = setTimeout(startTimer, delay); //вызываем функцию, 3000 - задержка 3 секудны
+
+//alert('Alert!!!');
+
+//clearTimeout(timer);  // останавливаем таймер
+
+*/
+
