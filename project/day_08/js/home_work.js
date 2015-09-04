4287dfea8dffc4cd771cdfe2a343faf9d5416d52
Ivan Simeyko <iv.simeyko@gmail.com>
2015-09-04 23:01:26 +0300

    День 08 - разобрал функцию для добавления автора

--- project/day_08/js/home_work.js
+++ project/day_08/js/home_work.js
@@ -1,32 +1,52 @@
 // Practics
 
+// Создадим заголовок для формы
+var tagP = document.createElement('p');
+tagP.align = 'center';
+tagP.innerHTML = '<strong> Выбераем автора новой книги </strong>';
+console.log( 'Создаем заголовок моей формы');
+document.body.appendChild(tagP);
+
+// Создаем массив для авторов
 var authorApi = (function () {
     var author = [];
+
+    // Создаем контейнер ul
     var ulObj = document.createElement('ul');
+
+    // Конструктор для автора
     function Author (authorName, authorSurname) {
         this.name = authorName;
         this.surname = authorSurname;
     }
 
+   /* // для определения строчных элементов сощдаем тег <span>
     function createSpan () {
 		var spanObj = document.createElement('span');
-
     }
+*/
+    //Функция для добавления автора
     function renderAuthor (element, index, array) {
 		var liObj = document.createElement('li'),
-			spanName = document.createElement('span'),
-			spanSurname = document.createElement('span'),
-			removeButton = document.createElement('button');
-		spanName.innerHTML = element.name + ', ';
-		spanSurname.innerHTML = element.surname + ' - ';
-		removeButton.innerHTML = 'x';
+		// Создаем текстовы поля и кнопочку
+        spanName = document.createElement('span'),
+		spanSurname = document.createElement('span'),
+		removeButton = document.createElement('button');
+        // Заполняем эти поля значениями
+        spanName.innerHTML = element.name + ', ';
+		spanSurname.innerHTML = element.surname + '  ';
+		removeButton.innerHTML = 'delete author';
+        // Вешаем на кнопочку событие
 		removeButton.addEventListener('click', removeBook.bind(null, element, index, liObj));
-		liObj.appendChild( spanName );
+        // Добавлем автора в документ
+        liObj.appendChild( spanName );
 		liObj.appendChild( spanSurname );
 		liObj.appendChild( removeButton );
 		ulObj.appendChild( liObj );
+        console.log('добавил автора', ulObj)
     }
 
+    //Пишем функцию, которая будет удалять нашего автора
     function removeBook(book, index, bookLi) {
 		author.splice(author.indexOf(author), 1);
 		authorLi.parentNode.removeChild(authorLi);
