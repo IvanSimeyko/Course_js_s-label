693429d9dfea47f625a086787eb90621052af8c9
Ivan Simeyko <iv.simeyko@gmail.com>
2015-09-13 21:49:40 +0300

    День 10 - создал файлы для домашки (работа в формой)

---
+++ project/day_10/js/home_work.js
@@ -0,0 +1,12 @@
+// решение домашки
+
+$(document).ready( function(){
+        // Находим объекты
+    $message = $('.form-control');
+    console.log($message);
+
+    //Создаем действие на действие мыши
+    var mes = $message.val();
+    console.log( 'mes')
+
+});
