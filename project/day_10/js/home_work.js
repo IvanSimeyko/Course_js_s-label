72b40344e1f56c9e0240db32b9c9b2797c18fa6f
Ivan Simeyko <iv.simeyko@gmail.com>
2015-09-15 23:26:40 +0300

    День 10 - добавил первое условие

--- project/day_10/js/home_work.js
+++ project/day_10/js/home_work.js
@@ -1,29 +1,19 @@
 // решение домашки
 
-$(document).ready( function(){
+$(document).ready(function () {
 
-    // Находим объекты
-    /*$message = $('.form-control');
-    console.log($message);
-*/
-    //Вешаем событие на нажатие enter
-    /*$message.submit(function( event ) {
-        console.log( 'was Enter' );
-        if ($message.val().length > 0) {
-            event.preventDefault();
-            var mes = $message.val();
-            console.log( 'mes' );
-        }
-    })*/
-    // Simple example
-    $('.form-control').submit(function(event) {
-        console.log( 'was Enter' );
-        var mes = $('.form-control').val();
-        console.log( mes );
-        //return false
-    });
-
-    $('.form-control').submit();
+    var container = $('.main ul');
+   $('.header').submit(function (e) {
+       e.preventDefault();
+       var value = $.trim($('#foo').val());
+       if (value) {
+           container.append('<div><label><input type="checkbox">' + value + '</label><div>');
+       }
+   });
 });
 
-//
+
+
+
+
+
