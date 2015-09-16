f2b299ff27c96d54749c15330665807aa3cb90fb
Ivan Simeyko <iv.simeyko@gmail.com>
2015-09-16 23:06:54 +0300

    День 10 - добавил очитску формы

--- project/day_10/js/home_work.js
+++ project/day_10/js/home_work.js
@@ -2,18 +2,24 @@
 
 $(document).ready(function () {
 
-    var container = $('.main ul');
+   var container = $('.main ul');    // находим элемент куда будем вставлять новые данные
+
    $('.header').submit(function (e) {
-       e.preventDefault();
-       var value = $.trim($('#foo').val());
+       console.log( 'was enter');
+
+       e.preventDefault();    // отмена действия браузера по-умолчанию
+       var value = $.trim($('#foo').val());   // trim удаляет пустые места в начале и в конце строки
+
        if (value) {
-           container.append('<div><label><input type="checkbox">' + value + '</label><div>');
+           container.append('<li><label><input type="checkbox">' + value + '</label></li>');
+           console.log( 'очищаем поле');
+           $('#foo').val('');    // очистил поле
        }
    });
+
+   $("a[href$='complited']").click(function(e) {    // находим ссылку "завершенные" и ставим
+       console.log( 'was click on "Завершенные"');                          // на нее событие
+
+       e.preventDefault();
+   })
 });
-
-
-
-
-
-
