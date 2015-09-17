293fb8bcfd5f406234237a11cee777a500bc03ab
Ivan Simeyko <iv.simeyko@gmail.com>
2015-09-17 07:23:25 +0300

    День 10 - добавил действие на ссылку "Все"

--- project/day_10/js/home_work.js
+++ project/day_10/js/home_work.js
@@ -4,22 +4,44 @@ $(document).ready(function () {
 
    var container = $('.main ul');    // находим элемент куда будем вставлять новые данные
 
-   $('.header').submit(function (e) {
-       console.log( 'was enter');
+    //обработка ввода задачи
+    $('.header').submit(function (e) {
+        console.log( 'was enter');
 
-       e.preventDefault();    // отмена действия браузера по-умолчанию
-       var value = $.trim($('#foo').val());   // trim удаляет пустые места в начале и в конце строки
+        e.preventDefault();    // отмена действия браузера по-умолчанию
+        var value = $.trim($('#foo').val());   // trim удаляет пустые места в начале и в конце строки
 
-       if (value) {
-           container.append('<li><label><input type="checkbox">' + value + '</label></li>');
-           console.log( 'очищаем поле');
-           $('#foo').val('');    // очистил поле
-       }
-   });
+        if (value) {
+            container.append('<li><label><input type="checkbox">' + value + '</label></li>');
 
-   $("a[href$='complited']").click(function(e) {    // находим ссылку "завершенные" и ставим
-       console.log( 'was click on "Завершенные"');                          // на нее событие
+            console.log( 'очищаем поле');
+            $('#foo').val('');    // очистил поле формы
+        }
+    });
 
-       e.preventDefault();
-   })
+    // обработка клика на завершенные
+    $("a[href$='complited']").click(function(e) {    // находим ссылку "завершенные" и ставим
+        console.log( 'was click on "Завершенные"');      //   на нее событие
+
+        e.preventDefault();
+
+        $('input[type="checkbox"]:not(:checked)').each(function(index, element) {    // выполняем обход всех инпутов
+                                                                                    // выполняем для каждого из
+                                                                                    // них функцию function
+        $(element).closest('li').hide();   // ищем ближайшую лишку и прячем ее
+       });
+    });
+
+    // обработка клика на "на все"
+    $('.selected').click(function(e) {
+        console.log( 'was click on "Все"');
+
+        e.preventDefault();
+
+        $('input[type="checkbox"]').each(function(index, element) {
+            $(element).closest('li').show();
+        });
+    });
+
+
 });
