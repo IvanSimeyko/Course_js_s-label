9dd7ba8086514526c18ee54d39db6c72410c5a40
IvanSimeyko <iv.simeyko@gmail.com>
2015-11-07 19:12:20 +0200

    start tic tac toe script

---
+++ project/day_18-tic_tac_toe/js/gamescript.js
@@ -0,0 +1,22 @@
+// сощдаем общие переменные
+var gameImgDir = 'data/';
+
+var gTexts =  [];
+gTexts['win1'] = 'Win ';
+gTexts['win2'] = '!';
+gTexts['playing'] = '...';
+gTexts['start'] = 'Begin game!';
+
+console.log(gTexts);
+console.log(gTexts['start']);
+
+var gField = [];    // игровое поле
+
+// задаем размер игрового поля
+function createField(w, h) {
+    gField = new Array(w);    // создаем новый массив
+    console.log( gField )
+    for ( i=0; i<w; i++ ) gField[i] = new Array(h);
+    console.log(gField)
+}
+createField(6, 2);
